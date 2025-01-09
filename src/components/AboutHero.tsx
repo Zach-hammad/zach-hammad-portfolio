7f9181ec75bbc1b4b2e13b223f540c116bd907ae
Zach-hammad <167034759+Zach-hammad@users.noreply.github.com>
2025-01-09 16:18:34 -0500

    nAME

--- src/components/AboutHero.tsx
+++ src/components/AboutHero.tsx
@@ -5,7 +5,7 @@ const AboutHero = () => {
     <div className="bg-primary-dark/5 dark:bg-primary-dark py-20 px-4">
       <div className="container max-w-4xl mx-auto text-center">
         <h1 className="text-4xl md:text-5xl font-bold text-foreground mb-6 animate-slide-up">
-          About Me
+          Zacharia Hammad
         </h1>
         <p className="text-lg text-muted-foreground max-w-2xl mx-auto animate-slide-up mb-6">
         I am a Computer Engineering student at Drexel University, graduating in June 2025. Outside academics, I practice Brazilian Jiu-Jitsu and assist in teaching children’s classes. I am passionate about traveling, photography, and chess, where my team won state and national championships.
